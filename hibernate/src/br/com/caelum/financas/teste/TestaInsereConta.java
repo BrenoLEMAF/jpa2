7c5ae166dc1b23a1f5494230adf0ee2201465d2e
Breno Zeymer <breno.lemaf@gmail.com>
2016-03-23 17:39:12 -0300

    exercicios opcionais

--- hibernate/src/br/com/caelum/financas/teste/TestaInsereConta.java
+++ hibernate/src/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -1,5 +1,7 @@
 package br.com.caelum.financas.teste;
 
+import java.awt.SystemColor;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -9,19 +11,24 @@ import br.com.caelum.financas.modelo.Conta;
 public class TestaInsereConta {
 
 	public static void main(String[] args) {
+		long inicio = System.currentTimeMillis();
+		
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("controlefinancas");
 		EntityManager entityManager = factory.createEntityManager();
 		
 		Conta conta = new Conta();
 		conta.setTitular("Raphael Rios Zeymer");
 		conta.setBanco("Itaú");
-		conta.setNumero("456789-2");
-		conta.setAgencia("3216");
+		conta.setNumero("709388-9");
+		conta.setAgencia("8347");
 		
 		entityManager.getTransaction().begin();
 		entityManager.persist(conta);
 		entityManager.getTransaction().commit();
 		
+		long fim = System.currentTimeMillis();
+		
 		System.out.println("Conta gravada com sucesso!");
+		System.out.println("Executado em: " + (fim - inicio) + " millissegundos");
 	}
 }
